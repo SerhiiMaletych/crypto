b9a7210c1ffaed47629b1b0debe8608f1a1ea4b2
SerhiiMaletych <maitrewop@gmail.com>
2020-05-04 17:02:06 +0300

    17 tests for vigenerecodec

---
+++ src/test/java/petApp/crypto/VigenereCodecTest.java
@@ -0,0 +1,40 @@
+package petApp.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import petApp.crypto.coders.VigenereCodec;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class VigenereCodecTest {
+    @Autowired
+    VigenereCodec vigenereCodec;
+
+    @Test
+    public void testEncode() {
+        String expected = "igmnp";
+        assertEquals(vigenereCodec.encode("hello"), expected);
+
+    }
+
+    @Test
+    public void testDecode() {
+        String expected = "hello";
+        assertEquals(vigenereCodec.decode("igmnp"), expected);
+    }
+
+    @Test
+    public void testEncodeMultipleWords() {
+        String expected = "igmnpbxqsne";
+        assertEquals(vigenereCodec.encode("hello world"), expected);
+
+    }
+
+    @Test
+    public void testDecodeMultipleWords() {
+        String expected = "hello world";
+        assertEquals(vigenereCodec.decode("igmnpbxqsne"), expected);
+    }
+}
