41aee220b952c623b696bdd4f347f072a800bc07
SerhiiMaletych <maitrewop@gmail.com>
2020-05-04 17:00:03 +0300

    16 added VigenereCodec

--- src/main/java/petApp/crypto/util/CodecUnsupportedException.java
+++ src/main/java/petApp/crypto/util/CodecUnsupportedException.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 @Component
 public class CodecUnsupportedException extends Exception {
     private Logger logger = Logger.getLogger("MyLog");
+
     public CodecUnsupportedException() {
         logger.warning("Wrong codec, please try one more time");
         System.out.println(".....................................");
--- src/main/java/petApp/crypto/util/OperationUnsupportedException.java
+++ src/main/java/petApp/crypto/util/OperationUnsupportedException.java
@@ -7,11 +7,11 @@ import java.util.logging.Logger;
 @Component
 public class OperationUnsupportedException extends Exception {
     private Logger logger = Logger.getLogger("MyLog");
+
     public OperationUnsupportedException() {
         logger.warning("You have chosen wrong operation, please try one more time");
         System.out.println("...............................");
     }
-
 
 
 }
