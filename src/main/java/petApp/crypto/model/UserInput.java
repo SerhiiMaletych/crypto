620ae6d79f4f63c7f7bddb06fb7353cd52edf46b
SerhiiMaletych <maitrewop@gmail.com>
2020-05-09 15:10:58 +0300

    35 added user input class

---
+++ src/main/java/petApp/crypto/model/UserInput.java
@@ -0,0 +1,24 @@
+package petApp.crypto.model;
+
+public class UserInput {
+    private long id;
+    private String input;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+
+}
