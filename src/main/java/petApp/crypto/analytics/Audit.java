1a861d9dc47b7a066e138021773bcb126508c7bb
SerhiiMaletych <maitrewop@gmail.com>
2020-05-11 17:08:01 +0300

    41 added Audit class and 3 empty methods

---
+++ src/main/java/petApp/crypto/analytics/Audit.java
@@ -0,0 +1,14 @@
+package petApp.crypto.analytics;
+
+public class Audit {
+    public String countEncodingInputs() {
+        return null;
+    }
+
+    public String countCodingsByDate() {
+        return null;
+    }
+    public String findMostPopularCodec() {
+        return null;
+    }
+}
