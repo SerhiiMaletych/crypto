413a73c62e84aafd87ff69547edcb87027b61fa2
SerhiiMaletych <maitrewop@gmail.com>
2020-10-16 23:24:28 +0300

    #48 - some deleting

--- src/test/java/pet_app/crypto/controller/ControllerTestForNull.java
+++ src/test/java/pet_app/crypto/controller/ControllerTestForNull.java
@@ -1,39 +1,39 @@
-package pet_app.crypto.controller;
-
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import pet_app.crypto.controller.codec.CodecTemplateController;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ControllerTestForNull {
-
-
-    @Autowired
-    CodecTemplateController codecController;
-    @Autowired
-    LoginController loginController;
-    @Autowired
-    MainController mainController;
-
-
-    @Test
-    public void CodecControllerNotNull() {
-        assertThat(codecController).isNotNull();
-    }
-
-    @Test
-    public void LoginControllerNotNull() {
-        assertThat(loginController).isNotNull();
-    }
-
-    @Test
-    public void main_controller_not_null() {
-        assertThat(mainController).isNotNull();
-    }
-
-
-}
+//package pet_app.crypto.controller;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import pet_app.crypto.controller.codec.CodecTemplateController;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class ControllerTestForNull {
+//
+//
+//    @Autowired
+//    CodecTemplateController codecController;
+//    @Autowired
+//    LoginController loginController;
+//    @Autowired
+//    MainController mainController;
+//
+//
+//    @Test
+//    public void CodecControllerNotNull() {
+//        assertThat(codecController).isNotNull();
+//    }
+//
+//    @Test
+//    public void LoginControllerNotNull() {
+//        assertThat(loginController).isNotNull();
+//    }
+//
+//    @Test
+//    public void main_controller_not_null() {
+//        assertThat(mainController).isNotNull();
+//    }
+//
+//
+//}
