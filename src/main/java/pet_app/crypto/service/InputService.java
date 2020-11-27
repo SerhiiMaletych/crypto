b77253d44c113f43c0c5a1fb0f5ed841367a4b9d
SerhiiMaletych <maitrewop@gmail.com>
2020-11-27 22:20:51 +0200

    #76 - add service class for input, some style changes

--- src/main/java/pet_app/crypto/service/InputService.java
+++ src/main/java/pet_app/crypto/service/InputService.java
@@ -1,4 +1,37 @@
 package pet_app.crypto.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pet_app.crypto.model.Input;
+import pet_app.crypto.repository.InputRepository;
+
+import java.util.List;
+
+@Service
 public class InputService {
+
+    final
+    InputRepository repository;
+
+    public InputService(InputRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Input findById(Long id) throws Exception {
+        return repository.findById(id).orElseThrow(() -> new Exception("Input not found"));
+    }
+
+    public List<Input> findAll() {
+        return repository.findAll();
+
+    }
+
+    public Input saveInput(Input input) {
+        return repository.save(input);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
