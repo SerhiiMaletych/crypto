e456e772e8fa077e205e22bf9ebafecfd41f0c38
SerhiiMaletych <maitrewop@gmail.com>
2020-11-30 21:47:26 +0200

    #82 - add first native query, add simple analytics rest controller

--- src/main/java/pet_app/crypto/controller/AnalyticsController.java
+++ src/main/java/pet_app/crypto/controller/AnalyticsController.java
@@ -1,9 +1,34 @@
 package pet_app.crypto.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pet_app.crypto.repository.InputRepository;
 
-@Controller
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 //@PreAuthorize("hasAuthority('admin_rights')")
+@RequestMapping("analytics")
 public class AnalyticsController {
+
+    final InputRepository repository;
+
+
+    public AnalyticsController(InputRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/most_used_codec")
+    public String mostUsedCodec() {
+        return  repository.findMostUsedCodec();
+    }
+
+
+
+
 }
