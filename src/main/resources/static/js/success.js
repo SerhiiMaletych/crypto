19b554e3dfdc034a83141ec46d13ff6859c83cf0
SerhiiMaletych <maitrewop@gmail.com>
2020-12-03 21:34:25 +0200

    #91 - add js alerts for submit buttons in security layer

---
+++ src/main/resources/static/js/success.js
@@ -0,0 +1,15 @@
+function confirmRegister() {
+    const register = document.forms[0].register.value;
+    alert("Thanks for registration! Now you will be redirected to main page");
+}
+
+function confirmLogin() {
+    const login = document.forms[0].login.value;
+    alert("Thanks for login! Now you will be redirected to user-info page");
+}
+
+function confirmLogout() {
+    const logout = document.forms[0].logout.value;
+    alert("You have logged out.");
+}
+
